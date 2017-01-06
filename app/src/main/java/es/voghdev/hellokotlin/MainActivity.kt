76c4e0b96e6ee4a5a26221a42adf8cbf68a135a3
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-01-06 20:05:17 +0100

    Add Kotlin plugins and anko, impl devexperto's example

--- app/src/main/java/es/voghdev/hellokotlin/MainActivity.kt
+++ app/src/main/java/es/voghdev/hellokotlin/MainActivity.kt
@@ -2,11 +2,15 @@ package es.voghdev.hellokotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener { toast("You wrote: ${editText1.text}") }
     }
 }
