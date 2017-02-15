b6a6b4cc8de7b92bec92bc483a283a6e9ab031af
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-02-15 20:29:46 +0100

    Reformat

--- app/src/main/java/es/voghdev/hellokotlin/MainActivity.kt
+++ app/src/main/java/es/voghdev/hellokotlin/MainActivity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 Olmo Gallegos Hernández.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package es.voghdev.hellokotlin
 
 import android.app.Activity
@@ -29,17 +44,16 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
     }
 
     fun Context.screenWidth(): Int {
-        val displayMetrics : DisplayMetrics = DisplayMetrics()
+        val displayMetrics: DisplayMetrics = DisplayMetrics()
         (this as Activity).windowManager.defaultDisplay.getMetrics(displayMetrics)
         return displayMetrics.widthPixels
     }
 
     fun Context.screenHeight(): Int {
-        val displayMetrics : DisplayMetrics = DisplayMetrics()
+        val displayMetrics: DisplayMetrics = DisplayMetrics()
         (this as Activity).windowManager.defaultDisplay.getMetrics(displayMetrics)
         return displayMetrics.heightPixels
     }
