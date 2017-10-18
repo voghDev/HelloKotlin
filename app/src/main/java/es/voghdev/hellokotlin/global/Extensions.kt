685dadaa08ab285d0992a29e81a109340d138cce
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-10-18 17:28:53 +0200

    Lol cmon

--- app/src/main/java/es/voghdev/hellokotlin/global/Extensions.kt
+++ app/src/main/java/es/voghdev/hellokotlin/global/Extensions.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.appandweb.peep.global
+package es.voghdev.hellokotlin.global
 
 import android.app.Activity
 import android.support.v4.content.ContextCompat
